091e0b60cdbe932672c6b14a6d04fc65a39433fb
root <root@localhost.localdomain>
2021-08-18 20:15:43 +0000

    added scripts

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt-get update -y
 apt-get upgrade -y
-apt-get install zsh git curl nano ufw -y 
+apt-get install zsh git curl nano ufw htop net-tools -y 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 
